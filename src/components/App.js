8c09e654e2a8e654ff10d8175fdf4dbc75e0f441
prabha666 <108609740+prabha666@users.noreply.github.com>
2022-10-04 22:25:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,30 +9,43 @@ import React, { useState, useRef } from 'react';
 
 function App() {
 
-   const fnameRef = useRef(null)
-   const emailRef = useRef(null)
+ /**
+  * code here
+  */
+   const fnameRef = useRef();
+  const emailRef = useRef();
+  const [error, setError] = useState(undefined);
+  const [data, setData] = useState({ fname: undefined, lname: undefined });
 
-  const [error, setError] = useState(null)
-  const validateEmail = (e) => {   
-    const email = emailRef.current.value
-    if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-      setError(null)
+  const change = () => {
+    // console.log(emailRef.current.value);
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailRef.current.value)) {
+      setError(undefined);
+      document.getElementById("submit").disabled = false;
     } else {
-      setError('Email is invalid')
+      setError("Email is invalid");
+      document.getElementById("submit").disabled = true;
     }
-  }
+  };
+        
 
   return(
     <div className="App">
       <h1>How About Them Apples</h1>
-      <form onSubmit = {validateEmail}>
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          setData({
+            fname: fnameRef.current.value,
+            lname: emailRef.current.value
+          });
+        }}>
         <fieldset>
           <label>
             <p>First Name</p>
             <input id='fname' name="name"  ref={fnameRef}/>
             <br></br>
             <p>Email</p>
-            <input id='lname' name="name"   ref={emailRef} />
+            <input id='lname' name="name" onChange={change}  ref={emailRef}/>
             {error && <h2 style={{color: 'red'}}>{error}</h2>}
           </label>
         </fieldset>
@@ -40,12 +53,12 @@ function App() {
         <button id='submit' type="submit">Submit</button>
       </form>
       {
-//         data.fname !== undefined && (
-//           <div>
-//           <h1>{data.fname}</h1>
-//           <h2>{data.lname}</h2>
-//           </div>
-//         )
+        data.fname != undefined && (
+          <div>
+          <h1>{data.fname}</h1>
+          <h2>{data.lname}</h2>
+          </div>
+        )
       }
     </div>
   )
